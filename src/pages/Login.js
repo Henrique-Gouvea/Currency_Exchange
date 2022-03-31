6c371767c48bb5a898cb0de90e6a982a79b63f3b
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-03-31 16:43:23 -0300

    add input e fazendo validaçao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,8 +3,49 @@ import { connect } from 'react-redux';
 import { userAction } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      btn: true,
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    const { email, password } = this.state;
+    const regex = /\S+@\S+\.\S+/; // regex https://www.horadecodar.com.br/2020/09/07/expressao-regular-para-validar-e-mail-javascript-regex/
+    const fiveValidation = 5;
+    if ((regex.test(email)) && (password.length > fiveValidation)) {
+      this.setState({ btn: false });
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, btn } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        <input
+          type="text"
+          onChange={ this.handleChange }
+          placeholder="E-mail"
+          data-testid="email-input"
+          value={ email }
+          name="email"
+        />
+        <input
+          type="password"
+          onChange={ this.handleChange }
+          placeholder="Senha"
+          data-testid="password-input"
+          value={ password }
+          name="password"
+        />
+        <button type="submit" disabled={ btn }>Entrar</button>
+      </div>);
   }
 }
 
